c692211f70edec7d52cd320d12b2e4c879be1bf6
Vadisys <vanaja@vadisys.com>
2022-10-13 03:54:53 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,22 @@
+# _*_ coding: utf-8 _*_
+{
+    'name' : 'Odoo Academy',
+    'summary' : """Academy app to manage training""",
+    'description' : """
+    
+    Academy Module to manage training:
+    - Courses
+    - Sessions
+    - Attendees
+    """,
+    'author' : 'Odoo',
+    'website' : 'https://www.odoo.com',
+    'category' : 'Training',
+    'depends': ['base'],
+    'data': [
+    ],
+    
+    'demo' : [
+        
+    ],
+}
