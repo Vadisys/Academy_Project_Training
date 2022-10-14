746b326a37963934faf352a98b53b7d4e5ff0564
Vadisys <Vanaja@vadisys.com>
2022-10-14 21:04:43 +0000

    [ADD] Basic Module

---
+++ cooperative_volunteers/__manifest__.py
@@ -0,0 +1,25 @@
+# _*_ coding : UTF-8 _*_
+{
+    'name' : 'Cooperative Volunteers',
+    'summary' : """Cooperative Volunteers app to work with voluneteers""",
+    'description' : """
+    
+    Cooperative to manage volunteers:
+    - Volunteers
+    - Shop
+    
+    """,
+    'author' : 'Odoo',
+    'website' : 'https://www.odoo.com',
+    'category' : 'Training',
+    'version' : '0.1',
+    'depends': ['base'],
+    'data': [
+    ],
+    
+    'demo' : [
+        
+    ],
+    
+    'license' : 'OPL-1',
+}
---
+++ library_management/__manifest__.py
@@ -0,0 +1,25 @@
+# _*_ coding : UTF-8 _*_
+{
+    'name' : 'Library Management',
+    'summary' : """Space Mission app to manage Books and Customers """,
+    'description' : """
+    
+    Library Management Module to manage Books and Customers:
+    - Books
+    - Rentals
+    
+    """,
+    'author' : 'Odoo',
+    'website' : 'https://www.odoo.com',
+    'category' : 'Training',
+    'version' : '0.1',
+    'depends': ['base'],
+    'data': [
+    ],
+    
+    'demo' : [
+        
+    ],
+    
+    'license' : 'OPL-1',
+}
